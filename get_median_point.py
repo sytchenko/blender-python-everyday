088783fb8b8fe445fe380d71348b12b223989b49
sytchenko <77635715+sytchenko@users.noreply.github.com>
2021-08-30 02:09:50 +0300

    Create get_median_point.py

---
+++ get_median_point.py
@@ -0,0 +1,17 @@
+import bpy
+from mathutils import Vector
+
+#Get Median Point From selected Vertices
+
+def medianPoint (obj):
+	me = obj.data
+	verts_sel = [v.co for v in me.vertices if v.select]
+	if len(verts_sel)>0:
+		pivot = sum(verts_sel, Vector()) / len(verts_sel)
+		return pivot
+	else:
+		return "Vertices not selected"
+
+obj = bpy.context.object
+median = medianPoint (obj)
+print (median)
